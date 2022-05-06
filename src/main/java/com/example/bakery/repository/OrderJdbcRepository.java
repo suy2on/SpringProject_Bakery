21339066178975b1b0fec1c2bbd3694034df8b53
suy2on <tnwjdrms@naver.com>
2022-05-06 16:55:23 +0900

    # feat : order repository 작성

--- src/main/java/com/example/bakery/repository/OrderJdbcRepository.java
+++ src/main/java/com/example/bakery/repository/OrderJdbcRepository.java
@@ -6,24 +6,23 @@ import static com.example.bakery.Utils.toUUID;
 import com.example.bakery.model.Order;
 import com.example.bakery.model.OrderItem;
 import com.example.bakery.model.OrderStatus;
-import com.example.bakery.model.Product;
-import com.example.bakery.model.ProductCategory;
 import java.nio.charset.StandardCharsets;
+import java.sql.ResultSet;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.Optional;
 import java.util.UUID;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.dao.DataAccessException;
-import org.springframework.dao.EmptyResultDataAccessException;
-import org.springframework.dao.IncorrectResultSizeDataAccessException;
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public class OrderJdbcRepository implements OrderRepository {
 
     private static final Logger logger = LoggerFactory.getLogger(OrderJdbcRepository.class);
@@ -39,9 +38,23 @@ public class OrderJdbcRepository implements OrderRepository {
     // TODO : rollback문제
 
     @Override
+    public List<Order> getAllOrder(){
+        String sql1 = "select * from product_order";
+        String sql2 = "select * from order_item where order_id = UUID_TO_BIN(:orderId)";
+        List<Order> orders =jdbcTemplate.query(sql1, orderRowMapper);
+
+        orders.forEach(order ->{
+            List<OrderItem> orderItems = jdbcTemplate.query(sql2, Collections.singletonMap("orderId", order.getOrderId().toString().getBytes()), orderItemRowMapper);
+            order.setOrderItems(orderItems);
+        });
+
+        return orders;
+    }
+
+    @Override
     public Order insert(Order order) {
-        String sql1 = "insert order(order_id, order_status, phone_number, address, price, created_at, updated_at"
-            + "values(UUID_TO_BIN(:orderId), :orderStatus, :phoneNumber, :address, :price, :createdAt, :updatedAt";
+        String sql1 = "insert product_order(order_id, order_status, phone_number, address, price, created_at, updated_at)"
+            + "values(UUID_TO_BIN(:orderId), :orderStatus, :phoneNumber, :address, :price, :createdAt, :updatedAt)";
 
         String sql2 = "insert order_item(product_id, order_id, count, price, created_at, updated_at)"
             + "values(UUID_TO_BIN(:productId), UUID_TO_BIN(:orderId), :count, :price, :createdAt, :updatedAt)";
@@ -55,7 +68,7 @@ public class OrderJdbcRepository implements OrderRepository {
         }
 
         try{
-            order.getOrderItems().forEach(item -> jdbcTemplate.update(sql2, orderItemParamsMap(item)));
+            order.getOrderItems().forEach(item -> jdbcTemplate.update(sql2, orderItemParamsMap(order, item)));
         } catch (DataAccessException e){
             logger.error("Got error during insert orderItem :",e);
             throw e;
@@ -64,14 +77,44 @@ public class OrderJdbcRepository implements OrderRepository {
         return order;
     }
 
-    private Map<String, Object> orderItemParamsMap(OrderItem orderItem){
+    @Override
+    public int deleteAllOrder(){
+        String sql = "delete from product_order";
+        return jdbcTemplate.update(sql, Collections.emptyMap());
+
+    }
+
+    private RowMapper<Order> orderRowMapper = (rs, rowNum) -> {
+        UUID orderId = toUUID(rs.getBytes("order_id"));
+        List<OrderItem> orderItems = new ArrayList<>();
+        OrderStatus orderStatus = OrderStatus.valueOf(rs.getString("order_status"));
+        String phoneNumber = rs.getString("phone_number");
+        String address = rs.getString("address");
+        int price = rs.getInt("price");
+        LocalDateTime createdAt = toLocalDateTime(rs.getTimestamp("created_at"));
+        LocalDateTime updatedAt = toLocalDateTime(rs.getTimestamp("updated_at"));
+
+        return new Order(orderId,orderItems,phoneNumber,address,price,orderStatus,createdAt,updatedAt);
+    };
+
+    private RowMapper<OrderItem> orderItemRowMapper = (rs, rowNum) -> {
+        UUID productId = toUUID(rs.getBytes("product_id"));
+        int count = rs.getInt("count");
+        int price = rs.getInt("price");
+        LocalDateTime createdAt = toLocalDateTime(rs.getTimestamp("created_at"));
+        LocalDateTime updatedAt = toLocalDateTime(rs.getTimestamp("updated_at"));
+
+        return new OrderItem(productId,count,price,createdAt,updatedAt);
+    };
+
+    private Map<String, Object> orderItemParamsMap(Order order,  OrderItem orderItem){
         HashMap<String, Object> objectHashMap = new HashMap<>();
-        objectHashMap.put("orderId", orderItem.getOrderId().toString().getBytes());
+        objectHashMap.put("orderId", order.getOrderId().toString().getBytes());
         objectHashMap.put("productId", orderItem.getProductId().toString().getBytes());
         objectHashMap.put("count", orderItem.getCount());
         objectHashMap.put("price", orderItem.getPrice());
-        objectHashMap.put("createdAt",orderItem.getCreatedAt());
-        objectHashMap.put("updatedAt",orderItem.getUpdatedAt());
+        objectHashMap.put("createdAt",order.getCreatedAt());
+        objectHashMap.put("updatedAt",order.getUpdatedAt());
 
         return objectHashMap;
     }
--- src/main/java/com/example/bakery/repository/OrderRepository.java
+++ src/main/java/com/example/bakery/repository/OrderRepository.java
@@ -10,7 +10,11 @@ import java.util.UUID;
 
 public interface OrderRepository {
 
+    List<Order> getAllOrder();
+
     Order insert(Order order);
+
+    int deleteAllOrder();
 
 
 }
